c0f48f80b100498844eade24d72b8ac6e0949a6c
edwinndungu2 <edwin.ndungu2@student.moringaschool.com>
2025-05-29 13:37:08 +0300

    added song.py

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,38 @@
 class Song:
-    pass
+    
+    count = 0
+    artists = []
+    genres = []
+
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+        Song.artists.append(artist)
+        Song.count += 1
+        Song.genres.append(genre)
+
+    @classmethod
+    def get_count(cls):
+        return cls.count
+
+    @classmethod
+    def get_genres(cls):
+        return list(set(cls.genres))
+
+    @classmethod
+    def get_artists(cls):
+        return list(set(cls.artists))
+
+    @classmethod
+    def genre_count(cls):
+        return {genre: cls.genres.count(genre) for genre in set(cls.genres)}
+
+    @classmethod
+    def artist_count(cls):
+        return {artist: cls.artists.count(artist) for artist in set(cls.artists)}
+    
+     
+
+
+test = Song("Lamba Lolo", "Ethic", "Gengetone")
